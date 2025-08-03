7d0b9f14fed3f507b476e700d84e9264f8d8af48
Koshikamani <turnipstack@gmail.com>
2025-08-03 23:14:40 +0530

    Auto commit: 2025-08-03 23:14:40

--- 238.product-of-array-except-self.java
+++ 238.product-of-array-except-self.java
@@ -7,20 +7,18 @@
 // @lc code=start
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int left[]=new int[nums.length];
-        int right[]=new int[nums.length];
-        left[0]=1;
-        right[nums.length-1]=1;
-        for(int i=0;i<nums.length;i++){
-            if(left[i]==0){
-                left[i]=1;
-            }
-            left[i]=left[i]*nums[i];
+        int arr[]=new int[nums.length];
+        int right=1;
+        arr[0]=1;
+        for(int i=1;i<nums.length;i++){
+            arr[i]=arr[i-1]*nums[i-1];
         }
-
         for(int i=nums.length-1;i>=0;i--){
-
+                arr[i]=arr[i]*right;
+                right=right*nums[i];
         }
+        return arr;
+
         
     }
 }
