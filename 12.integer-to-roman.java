f9ae91c4d4e14d49603826d0a4f0b08cb87dc078
Koshikamani <turnipstack@gmail.com>
2025-08-03 23:28:54 +0530

    Auto commit: 2025-08-03 23:28:54

--- 12.integer-to-roman.java
+++ 12.integer-to-roman.java
@@ -15,6 +15,7 @@ class Solution {
         m.put(1000,"M");
         m.put(500,"D");
         m.put(100,"C");
+        m.put(90,"XC");
         m.put(50,"L");
         m.put(40,"XL");
         m.put(10,"X");
