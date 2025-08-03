970847c10dbb7ece959ea2196599c2720190dd6f
Koshikamani <turnipstack@gmail.com>
2025-08-03 20:06:36 +0530

    Auto commit: 2025-08-03 20:06:36

--- 125.valid-palindrome.java
+++ 125.valid-palindrome.java
@@ -10,7 +10,7 @@ class Solution {
         StringBuilder sb=new StringBuilder();
         StringBuilder sc=new StringBuilder();
         for(int i=0;i<s.length();i++){
-            if(Character.isLetterOrDigit(0)s.charAt(i))
+            if(Character.isLetterOrDigit(s.charAt(i))
         }        
     }
 }
